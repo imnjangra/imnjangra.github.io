7e92fe68c360f03238c6193f1e20428a4e1a021c
N Jangra <imnavjotjangra@gmail.com>
2020-12-17 03:19:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -401,9 +401,19 @@ const showMainAnimation = (parent) => {
   box.classList.add("box", "flex");
   parent.appendChild(boxContainer);
   boxContainer.appendChild(box);
-  const quote = ["NAVJOT", "JANGRA"];
+  const subquote = ["The", "name", "is"];
+  const quote = ["NAVJOT", "JANGRA"];	
+	
   let delay = 0;
   // Add all the words
+	 for (let word in subquote){
+    let text = document.createElement("span");
+    text.textContent = quote[word];
+    text.classList.add("");
+    delay = delay + 22;
+    text.style.animationDelay = delay + "ms";
+    box.appendChild(text);}
+	
   for (let word in quote){
     let text = document.createElement("span");
     text.textContent = quote[word];
